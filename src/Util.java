b771f8efde03634d27351adf114426066a1e93ac
xiang_wang <xiang_wang@IntSig-pc0119.intsig.com>
2018-11-29 17:22:01 +0800

    清除所有DB所有TB的数据

--- src/Util.java
+++ src/Util.java
@@ -18,14 +18,12 @@ import java.util.Map;
 
 public class Util
 {
-     
+
   public static String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-  public static String DB_URL = "jdbc:mysql://127.0.0.1:3306/db_xxx";
+  public static String DB_URL = "jdbc:mysql://";
   
-  public static String USER = "xxx";
-  public static String PASS = "xxx";
-  
-  public static int tbcnt = 0;
+  public static String USER = "canal";
+  public static String PASS = "canal";
   
   public static void exportstructure(String userName, String path, String db) throws IOException, SQLException {
     BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(path)));
@@ -37,15 +35,17 @@ public class Util
     while ((ss = br.readLine()) != null)
     {
       if (ss.contains("CREATE TABLE")) {
-        tbcnt += 1;
         creatingField = true;
         String[] tbsplit = ss.split("`");
         currTable = tbsplit[1];
+
+
 
       }
       else if ((creatingField) && (ss.startsWith(")"))) {
         creatingField = false;
         currTable = "";
+
 
       }
       else if ((creatingField) && (!ss.contains("KEY"))) {
@@ -60,23 +60,18 @@ public class Util
     }
     
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(userName + "_" + db + "_fixed" + ".txt")));
-    for (Map<String, String> map_item : list) {
+    for (Map<String, String> map_item : list)
+    {
       map_item.replace("desc", ((String)map_item.get("desc")).replace("'", ""));
-      String tmpTbName = (String)map_item.get("tb");
-      if (!tmpTbName.matches(".*_[0-9]{0,1}[0-4]{0,1}$"))
-      {
-        if (tmpTbName.matches(".*_15"))
-        {
-          map_item.replace("tb", tmpTbName.substring(0, tmpTbName.lastIndexOf('_')) + "_" + tbSplitNum);
-        }
-        
-        bw.write((String)map_item.get("db") + "\t" + (String)map_item.get("tb") + "\t" + (String)map_item.get("field") + "\t" + (String)map_item.get("desc") + "\n");
-        String sql = "insert into tb_xxx set username = '" + userName + "', dbname = '" + (String)map_item.get("db") + "', tbname = '" + 
-          (String)map_item.get("tb") + "', fieldname = '" + (String)map_item.get("field") + "', fielddesc = '" + (String)map_item.get("desc") + "'";
-        System.out.println(sql);
-        executesql(sql);
-      }
+      bw.write((String)map_item.get("db") + "\t" + (String)map_item.get("tb") + "\t" + (String)map_item.get("field") + "\t" + (String)map_item.get("desc") + "\n");
+      String sql = "use " + map_item.get("db").toString();
+      System.out.println(sql);
+      executesql(sql);
+      sql = "delete from " + map_item.get("tb");
+      System.out.println(sql);
+      executesql(sql);
     }
+    
     br.close();
     bw.close();
   }
@@ -86,34 +81,35 @@ public class Util
   
   public static void initconn() throws Exception
   {
-    Class.forName("com.mysql.jdbc.Driver");
-    
-    System.out.println("连接数据库...");
-    conn = DriverManager.getConnection(DB_URL, USER, PASS);
-    
-    System.out.println(" 实例化Statement对象...");
+    Class.forName("com.mysql.cj.jdbc.Driver");
+    System.out.println(DB_URL);
+    System.out.println(USER);
+    System.out.println(PASS);
+    System.out.println("initconn...");
+    conn = DriverManager.getConnection(DB_URL, USER, PASS);    
     stmt = conn.createStatement();
   }
   
   public static void executesql(String sql) throws SQLException {
     stmt.executeUpdate(sql);
   }
-  
-  public static String dbSplitNum = "16";
-  public static String tbSplitNum = "16";
-  
 
   public static void main(String[] args)
     throws Exception
   {
-    
-    if (args.length != 2) {
-      System.out.println("error argus. only accept one param(directory path of *.sql)");
+    if (args.length != 4) {
+      System.out.println("error argus. only accept 4 params.");
       return;
     }
     
     String directoryPath = args[0];
-    String userName = args[1];
+    String hoString = args[1];
+    String userName = args[2];
+    String passWd = args[3];
+    System.out.println(directoryPath + hoString + userName + passWd);
+    USER = userName;
+    PASS = passWd;
+    DB_URL += hoString;
     
     LinkedList<File> linkedList = GetDirectory(directoryPath);
     System.out.println("fixing...");
@@ -122,19 +118,18 @@ public class Util
       String fPath = file.getPath();
       System.out.println("parsing " + fPath + "...");
       String dbName = fPath.substring(fPath.lastIndexOf(File.separatorChar) + 1, fPath.lastIndexOf('.'));
-      if (dbName.endsWith("_0"))
-      {
-
-        exportstructure(userName, fPath, dbName.replaceAll("_0", "_" + dbSplitNum));
-      }
-      else
-      {
+      if (dbName.endsWith("_0")) {
+        for (int i = 0; i < 16; i++)
+        {
+          exportstructure(userName, fPath, dbName.replaceAll("_0", "_" + i));
+        }
+        
+      } else {
         exportstructure(userName, fPath, dbName);
       }
     }
     
     conn.close();
-    System.out.println("tbcnt:" + tbcnt);
     System.out.println("finish...");
   }
   
